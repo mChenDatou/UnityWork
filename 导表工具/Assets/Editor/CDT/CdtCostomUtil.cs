292e9c823213f7c4fc1a9fba3c050a09aba339bb
Leeden <30721473+mChenDatou@users.noreply.github.com>
2026-05-06 14:19:27 +0800

    更新

--- 导表工具/Assets/Editor/CDT/CdtCostomUtil.cs
+++ 导表工具/Assets/Editor/CDT/CdtCostomUtil.cs
@@ -15,33 +15,52 @@ namespace CDT
             try
             {
                 string projectPath = Directory.GetParent(Application.dataPath).FullName;
-                string exePath = $"{projectPath}/excel2json/excel2json.exe";
+                string exePath = $"{projectPath}/excel2json/Excel2json.exe";
                 string configPath = $"{projectPath}/Assets/Config/";
-                string csPath = $"{projectPath}/Assets/Script/Config/";
+                string csPath = $"{projectPath}/Assets/Scripts/Config/";
+                
+                
                 configPath = configPath.Replace('/', Path.DirectorySeparatorChar);
                 csPath = csPath.Replace('/', Path.DirectorySeparatorChar);
                 exePath = exePath.Replace('/', Path.DirectorySeparatorChar);
+                
+                // 删除csPath路径下的所有文件
+                if (Directory.Exists(csPath))
+                {
+                    string[] csFiles = Directory.GetFiles(csPath);
+                    foreach (var csFile in csFiles)
+                    {
+                        File.Delete(csFile);
+                    }
+                    Debug.Log($"已清空目录: {csPath}");
+                }
+                
+                // 删除configPath路径下的所有文件
+                if (Directory.Exists(configPath))
+                {
+                    string[] cfFiles = Directory.GetFiles(configPath);
+                    foreach (var csFile in cfFiles)
+                    {
+                        File.Delete(csFile);
+                    }
+                    Debug.Log($"已清空目录: {configPath}");
+                }
                 
                 string[] excelFiles = Directory.GetFiles($"{projectPath}/Excel", "*.xlsx");
                 foreach (var excel in excelFiles)
                 {
                     string fileName = Path.GetFileNameWithoutExtension(excel);
-                    string cmdStr = $" -e {excel} -j {configPath}{fileName}.txt -p {csPath}{fileName}.cs -h 3 -a true";
-                    Debug.Log(cmdStr);
+                    string cmdStr = $" -e {excel} -j {configPath} -p {csPath} -h 3 -a true -x # -c true";
                     System.Diagnostics.Process.Start(exePath, @cmdStr);
                 }
+                Debug.Log("导出配置完成");
             }
             catch (System.Exception ex)
             {
                 Debug.LogError(ex.Message);
                 throw;
             }
-            AssetDatabase.Refresh();
-            Debug.Log("导出配置完成");
+            //AssetDatabase.Refresh();
         }
-        
-
-
     }
-
 }
