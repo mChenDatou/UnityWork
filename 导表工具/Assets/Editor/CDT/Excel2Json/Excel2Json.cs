6b0eb622342b6bdc35a8f4ac3b45971bd91ece08
Leeden <30721473+mChenDatou@users.noreply.github.com>
2026-05-11 18:06:52 +0800

    更新注释

--- 导表工具/Assets/Editor/CDT/Excel2Json/Excel2Json.cs
+++ 导表工具/Assets/Editor/CDT/Excel2Json/Excel2Json.cs
@@ -5,6 +5,19 @@ namespace CDT
 {
     public static class Excel2Json
     {
+        /*
+        -e, –excel Required. 输入的Excel文件路径.
+        -j, –json 指定输出的json文件路径.
+        -p, –csharp 指定输出的C#文件路径.
+        -h, –header (Default: 3)表格中有几行是表头.
+        -c, –encoding (Default: utf8-nobom) 指定编码的名称.
+        -l, –lowcase (Default: false) 自动把字段名称转换成小写格式.
+        -a 序列化成数组
+        -d, –date:指定日期格式化字符串，例如：dd / MM / yyy hh: mm:ss
+        -s 序列化时强制带上sheet name，即使只有一个sheet
+        -exclude_prefix： 导出时，排除掉包含指定前缀的表单和列，例如：-exclude_prefix #
+        -cell_json：自动识别单元格中的Json对象和Json数组，Default：false
+         */
         public static void Run(string excelPath, string exportJsonPath, string exportCsharpPath, int header = 3, string encoding = "\"utf8-nobom\"", string dateFormat = "\"yyyy-MM-dd\"", bool lowcase = false, bool exportArray = true, bool forceSheetName = false, string excludePrefix = "", bool cellJson = true, bool allString = false)
         {
             //-- Encoding
